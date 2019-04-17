59915fb27fbd7c25c683c8bc1fd6c8b843c176eb
Gregory Russo <gregoryjvrusso@gmail.com>
2019-04-17 00:12:52 -0300

    feat: implementação do menu

--- ecommerce/php/header.php
+++ ecommerce/php/header.php
@@ -13,18 +13,30 @@
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="produto-lista.php">Loja</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-login" href="#" id="navbarDropdownProduto" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Produtos
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownProduto">
+          <a class="dropdown-item" href="produto-cadastrar.php">Cadastrar</a>
+          <a class="dropdown-item" href="produto-lista.php">Listar</a>
+          <a class="dropdown-item" href="produto-lista-handcraft.php">Listar Segmentado</a>
+        </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-login" href="#" id="navbarDropdownUsuario" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Usuários
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownUsuario">
+          <a class="dropdown-item" href="usuario-cadastrar.php">Cadastrar</a>
+        </div>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -43,12 +55,17 @@
         <a class="dropdown-item" href="usuario-logout.php">Logout</a>
       </div>
     </li>
+    <?php }else{ ?>
+      <a class="navbar-brand text-login" id="login" href="login.php">Login</a>
+    <?php } ?>
     <style>
       .text-login{
         margin-left: 15px;
         color: #FFF !important;
       }
+      #login{
+        font-size: 15px;
+      }
     </style>
-    <?php } ?>
   </div>
 </nav>
